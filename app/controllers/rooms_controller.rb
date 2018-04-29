53696959b2969bb1c3950eab081a455118b870b7
Lili <marcela.b.abril@gmail.com>
2018-04-29 08:14:54 -0500

    adding deleting

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,13 @@ class RoomsController < ApplicationController
     end
   end
 
+   def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_path, notice: "La habitacion se elimino"
+  end
+
+
   
 
   protected
